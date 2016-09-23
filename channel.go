2f772ddac9e5f1c99aff56e3c1df360cc0c57c65
Jannick Fahlbusch <git@jf-projects.de>
2016-09-23 15:56:29 +0200

    Initial commit

---
+++ channel.go
@@ -0,0 +1,21 @@
+package logrusChannel
+
+import "github.com/Sirupsen/logrus"
+
+// ChannelHook is a hook for logrus
+type ChannelHook struct {
+	Channel chan *logrus.Entry
+}
+
+// NewChannelHook creates a hook to log to a channel
+func NewChannelHook(channel chan *logrus.Entry) (*ChannelHook, error) {
+	return &ChannelHook{
+		Channel: channel,
+	}, nil
+}
+
+// Fire is called whan a log event is fired
+func (hook *ChannelHook) Fire(entry *logrus.Entry) error {
+	hook.Channel <- entry
+	return nil
+}
